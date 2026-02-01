1d4b0d537159ee4b8ceb6c0b972a1b4e1c91ed47
Bryce Williams <bwilldonut@gmail.com>
2026-02-01 00:24:44 -0500

    Update HoverAnimation.cs
    added alternate OnHoverEnter approach
    
    gradually scales up + slightly tints item yellow; immediately scales back down to original size on exit

--- Assets/Scripts/HoverAnimation.cs
+++ Assets/Scripts/HoverAnimation.cs
@@ -2,19 +2,30 @@ using UnityEngine;
 
 public class HoverAnimation : MonoBehaviour
 {
-    float scaleFactor = 0.15f;
+    float scaleFactor = 0.2f;
+    float scaleRate = 0.005f;
+    Vector3 initialSize;
     SpriteRenderer mySprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         mySprite = GetComponent<SpriteRenderer>();
+        initialSize = transform.localScale;
     }
 
-    private void OnMouseEnter()
+    /*private void OnMouseEnter()
     {
         transform.localScale += new Vector3(scaleFactor, scaleFactor, 1);
         mySprite.material.color = Color.red;
+    }*/
+
+    private void OnMouseOver()
+    {
+        if (transform.localScale.x < initialSize.x + scaleFactor && transform.localScale.y < initialSize.y + scaleFactor) {
+            transform.localScale += new Vector3(scaleRate, scaleRate, 1);
+        }
+        mySprite.material.color = new Color32(255, 235, 200, 255);
     }
 
     private void OnMouseExit()
