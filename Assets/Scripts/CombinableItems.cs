daee898e38b53eea7a420972e9b71816fbb64eab
aeb687 <aeb687@nyu.edu>
2026-02-01 01:35:49 -0500

    Combinable items

---
+++ Assets/Scripts/CombinableItems.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class CombinableItem : PickupItems
+{
+    public List<PickupItems> combinors;
+    public List<CombinableItem> branches;
+
+    public override void OnMouseDown()
+    {
+        base.OnMouseDown();
+        foreach (PickupItems pickup in combinors)
+        {
+            if (pickup != null) pickup.gameObject.SetActive(false);
+        }
+        foreach (CombinableItem branch in branches)
+        {
+            if (branch != null) branch.gameObject.SetActive(false);
+        }
+    }
+
+    public override void Reset()
+    {
+        foreach (PickupItems pickup in combinors)
+        {
+            if (pickup != null && pickup is not CombinableItem) pickup.gameObject.SetActive(true);
+        }
+        this.gameObject.SetActive(false);
+    }
+
+}
