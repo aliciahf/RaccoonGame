7b4731227a1c520297d972aa3c4ee6b51e510865
aeb687 <aeb687@nyu.edu>
2026-02-01 17:30:39 -0500

    fix

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEditor;
-using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
